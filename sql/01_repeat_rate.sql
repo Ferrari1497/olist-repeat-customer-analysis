5cdebbc353737e0f20097740d3371f246dd3b113
Ferrari1497 <40347501+Ferrari1497@users.noreply.github.com>
2026-09-01 13:03:23 +0900

    Update 01_repeat_rate.sql

--- sql/01_repeat_rate.sql
+++ sql/01_repeat_rate.sql
@@ -1,18 +1,37 @@
--- リピート顧客数を算出
-SELECT COUNT(*)
+-- =========================================
+-- 1. 全顧客数を算出
+-- =========================================
+
+SELECT
+    COUNT(DISTINCT customer_unique_id) AS total_customers
+FROM olist_customers_dataset;
+
+
+-- =========================================
+-- 2. リピート顧客数を算出
+-- =========================================
+
+SELECT
+    COUNT(*) AS repeat_customers
 FROM (
     SELECT
-        olist_customers_dataset.customer_unique_id,
-        COUNT(olist_orders_dataset.customer_id)
-    FROM olist_orders_dataset
-    INNER JOIN olist_customers_dataset
-        ON olist_orders_dataset.customer_id
-        = olist_customers_dataset.customer_id
-    GROUP BY olist_customers_dataset.customer_unique_id
-    HAVING COUNT(olist_orders_dataset.customer_id) >= 2
-) AS repeat_customers;
+        customer_unique_id,
+        COUNT(order_id) AS order_count
+    FROM olist_customers_dataset
+    INNER JOIN olist_orders_dataset
+        ON olist_customers_dataset.customer_id
+        = olist_orders_dataset.customer_id
+    GROUP BY customer_unique_id
+    HAVING COUNT(order_id) >= 2
+) AS repeat_customer_data;
 
 
--- 全顧客数を算出
-SELECT COUNT(DISTINCT customer_unique_id) AS total_customers
-FROM olist_customers_dataset;
+/*
+分析結果
+
+全顧客数：96,096人
+リピート顧客数：2,997人
+リピート率：3.12%
+
+全顧客のうち、リピート顧客の割合は3.12%にとどまった。
+*/
