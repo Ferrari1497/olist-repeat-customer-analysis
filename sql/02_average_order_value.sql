924930197834a9d6cb718b3712448911e1af6331
Ferrari1497 <40347501+Ferrari1497@users.noreply.github.com>
2026-09-01 13:37:02 +0900

    Update 02_average_order_value.sql

--- sql/02_average_order_value.sql
+++ sql/02_average_order_value.sql
@@ -1,7 +1,17 @@
 -- =========================================
--- 1. 注文ごとの購入金額を算出
+-- 0. 既存の一時テーブルを削除
 -- =========================================
 
+DROP TABLE IF EXISTS order_payment_customer;
+DROP TABLE IF EXISTS customer_order_count;
+DROP TABLE IF EXISTS payment_by_order;
+
+
+-- =========================================
+-- 1. 注文ごとの購入金額を一時テーブルに保存
+-- =========================================
+
+CREATE TEMP TABLE payment_by_order AS
 SELECT
     order_id,
     SUM(payment_value) AS order_payment
@@ -10,9 +20,10 @@ GROUP BY order_id;
 
 
 -- =========================================
--- 2. リピート顧客を抽出
+-- 2. 顧客ごとの注文回数を一時テーブルに保存
 -- =========================================
 
+CREATE TEMP TABLE customer_order_count AS
 SELECT
     customer_unique_id,
     COUNT(order_id) AS order_count
@@ -20,23 +31,51 @@ FROM olist_customers_dataset
 INNER JOIN olist_orders_dataset
     ON olist_customers_dataset.customer_id
     = olist_orders_dataset.customer_id
-GROUP BY customer_unique_id
-HAVING COUNT(order_id) >= 2;
+GROUP BY customer_unique_id;
 
 
 -- =========================================
--- 3. 非リピート顧客を抽出
+-- 3. 注文金額に顧客情報を付与
+-- =========================================
+
+CREATE TEMP TABLE order_payment_customer AS
+SELECT
+    olist_orders_dataset.order_id,
+    olist_customers_dataset.customer_unique_id,
+    payment_by_order.order_payment
+FROM olist_orders_dataset
+INNER JOIN olist_customers_dataset
+    ON olist_orders_dataset.customer_id
+    = olist_customers_dataset.customer_id
+INNER JOIN payment_by_order
+    ON olist_orders_dataset.order_id
+    = payment_by_order.order_id;
+
+
+-- =========================================
+-- 4. リピート顧客の1注文あたり平均購入金額
 -- =========================================
 
 SELECT
-    customer_unique_id,
-    COUNT(order_id) AS order_count
-FROM olist_customers_dataset
-INNER JOIN olist_orders_dataset
-    ON olist_customers_dataset.customer_id
-    = olist_orders_dataset.customer_id
-GROUP BY customer_unique_id
-HAVING COUNT(order_id) = 1;
+    AVG(order_payment) AS avg_order_value
+FROM order_payment_customer
+INNER JOIN customer_order_count
+    ON order_payment_customer.customer_unique_id
+    = customer_order_count.customer_unique_id
+WHERE order_count >= 2;
+
+
+-- =========================================
+-- 5. 非リピート顧客の1注文あたり平均購入金額
+-- =========================================
+
+SELECT
+    AVG(order_payment) AS avg_order_value
+FROM order_payment_customer
+INNER JOIN customer_order_count
+    ON order_payment_customer.customer_unique_id
+    = customer_order_count.customer_unique_id
+WHERE order_count = 1;
 
 
 /*
